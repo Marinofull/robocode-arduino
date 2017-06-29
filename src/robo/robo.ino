753cb16680922ee3ecd8ef982a71d635f9634396
Pedro Dumont Marques <pedrodm@outlook.com>
2017-06-28 23:53:33 -0300

    atualizando para xbee

--- src/robo/robo.ino
+++ src/robo/robo.ino
@@ -3,36 +3,56 @@
   Authors:
   * Marino Hohenheim
   * Pedro Marques
-  Version: 0.1.0
+  Version: 0.2.0
   Boards Supported:
   * Arduino UNO*
   Dependencies:
+  * XBee-Arduino Library, 0.6.0
+  * XCTU, 6.3.8 (usado externamente para monitorar comunicação entre xbee e arduino)
   Description:
   Descrição do projeto
 */
 
 #include <string.h>
+#include <SoftwareSerial.h>
 
 #define MEM 50
+#define PORTARX 2
+#define PORTATX 3
 String msg;
 int mem[MEM];
 
+SoftwareSerial XBee(PORTARX, PORTATX);
+
 void setup(){
+    XBee.begin(9600);
     Serial.begin(9600);
     for(int i=0; i<MEM; i++)
         mem[i]=-1;
         /*
         Seta as coisa reservada
         */
+    delay(500);
+    if(XBee.isListening()) {
+      Serial.println("XBee está conectado!");
+    }
 }
 
 int parse_msg(String msg);
+int parse_msg_xbee(String msg);
 
 void loop(){
 
+    //Se tiver data sendo mandada pelo serial monitor
     if(Serial.available()){
         msg = String(Serial.readString());
         parse_msg(msg);
+    }
+
+    //Se tiver data vindo do XBee
+    if(XBee.available()) {
+        msg = String(XBee.readString());
+        parse_msg_xbee(msg);
     }
 }
 
@@ -56,6 +76,32 @@ int parse_msg(String msg){
             break;
         case 'M':
             Serial.println(&msg[1]);
+            break;
+        default:
+        break;
+    }
+    return 0;
+}
+
+int parse_msg_xbee(String msg){
+    char c = msg[0];
+    int p, value;
+
+    switch(c){
+        case 'G':
+            p = atoi(&msg[1]) -1;
+            XBee.print("R");//Response prefix
+            XBee.println(mem[p]);
+            break;
+        case 'S':
+            p = atoi(&msg[1]) -1;
+            value = atoi(&msg[msg.indexOf('=')+1]);
+            mem[p] = value;
+            XBee.print("R");//Response prefix
+            XBee.println(mem[p]);
+            break;
+        case 'M':
+            XBee.println(&msg[1]);
             break;
         default:
         break;
