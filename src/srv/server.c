81b0e8dd87bffbae8ab4e61ab5127b3f6986bea2
Marinofull <Marinofull@users.noreply.github.com>
2017-07-18 22:11:00 -0300

    terminal continuo

--- src/srv/server.c
+++ src/srv/server.c
@@ -6,43 +6,44 @@
 #include <string.h>
 
 int main(){
-    /* Porta serial do Xbee */
-    FILE *serial_port = fopen("/dev/ttyUSB1", "r+");
+  /* Porta serial do Xbee */
+  FILE *serial_port;
+  char s[200], device[11], c[2];
+
+  printf("Set device /dev/ttyUSB number -> ");
+  scanf("%s", c);
+  strcpy(device, "/dev/ttyUSB");
+  strcat(device, c);
+
+
+  for(;;){
+
+    serial_port = fopen(device, "r+");
 
     /* Se nulo a porta está sendo usada ou você não tem privilégios */
     if(serial_port == NULL){
-        printf("Error opening port");
-        return 1;
+      printf("Error opening port");
+      return 1;
     }
-
-    char c, s[1000];
-    int inteiro;
 
     /* Lê a entrada */
     printf("ENTRADA  -> ");
-    /*
-     * */
-    //scanf("%c%d", &c, &inteiro);
     scanf(" %[^\n]s%*c", s);
+    if(s[0] == 'e') return;
 
     /* Envia para o Xbee */
-    /*
-     * */
-    //fprintf(serial_port, "%c%d\n", c, inteiro);
     fprintf(serial_port, "%s\n", s);
 
     /* Recebe a resposta */
-    /*
-     * */
-    //fscanf(serial_port, "%c%d%*c", &c, &inteiro);
     fscanf(serial_port, "%s%*c", s);
 
     /* Imprime o resultado */
-    //printf("RESPOSTA -> %c%d\n", c, inteiro);
     printf("RESPOSTA -> %s\n", s);
 
     /* Fecha a porta serial */
     fclose(serial_port);
+    scanf("%s", c); //foi na sorte isso aqui
+  }
 
-    return 0;
+  return 0;
 }
