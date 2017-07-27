4f738007c3aa6cd27f5e14ae1bebdb4deec9a189
Marinofull <Marinofull@users.noreply.github.com>
2017-07-27 17:41:13 -0300

    add vendor

---
+++ src/vendor/baserobomoodle/baserobomoodle.ino
@@ -0,0 +1,78 @@
+#define ENCODER_DIREITA 2
+#define ENCODER_ESQUERDA 3
+
+#define ACELERADOR_DIREITA 5
+#define ACELERADOR_ESQUERDA 6
+
+//  Definição das entradas para os motores de cada lado, observem que alguns robôs precisaram ter os pinos trocados devido aos módulos de comunicação que utilizam.
+//  Vejam os 4 fios unidos da ponte H que são conectados no Arduino para saberem o padrão do robô que vocês tem em mãos.
+#define DIRECAO_DIREITA_1 7   //4
+#define DIRECAO_DIREITA_2 8   //7
+
+#define DIRECAO_ESQUERDA_1 9  //8
+#define DIRECAO_ESQUERDA_2 10 //9
+
+#define IR_PARA_FRENTE_DIREITA() do { digitalWrite(DIRECAO_DIREITA_1, HIGH); digitalWrite(DIRECAO_DIREITA_2, LOW); } while(false)
+#define IR_PARA_FRENTE_ESQUERDA() do { digitalWrite(DIRECAO_ESQUERDA_1, HIGH); digitalWrite(DIRECAO_ESQUERDA_2, LOW); } while(false)
+#define IR_PARA_FRENTE() do { IR_PARA_FRENTE_DIREITA(); IR_PARA_FRENTE_ESQUERDA(); } while(false)
+
+#define IR_PARA_TRAS_DIREITA() do { digitalWrite(DIRECAO_DIREITA_1, LOW); digitalWrite(DIRECAO_DIREITA_2, HIGH); } while(false)
+#define IR_PARA_TRAS_ESQUERDA() do { digitalWrite(DIRECAO_ESQUERDA_1, LOW); digitalWrite(DIRECAO_ESQUERDA_2, HIGH); } while(false)
+#define IR_PARA_TRAS() do { IR_PARA_TRAS_DIREITA(); IR_PARA_TRAS_ESQUERDA(); } while(false)
+
+#define ACELERA_DIREITA(VELOCIDADE) do { pwmDireita = VELOCIDADE; analogWrite(ACELERADOR_DIREITA, VELOCIDADE); } while(false)
+#define ACELERA_ESQUERDA(VELOCIDADE) do { pwmEsquerda = VELOCIDADE; analogWrite(ACELERADOR_ESQUERDA, VELOCIDADE); } while(false)
+#define ACELERA(VELOCIDADE) do { ACELERA_DIREITA(VELOCIDADE); ACELERA_ESQUERDA(VELOCIDADE); } while(false)
+
+#define FREIO_DIREITA() { ACELERA_DIREITA(0); digitalWrite(DIRECAO_DIREITA_1, LOW); digitalWrite(DIRECAO_DIREITA_2, LOW); } while(false)
+#define FREIO_ESQUERDA() { ACELERA_ESQUERDA(0); digitalWrite(DIRECAO_ESQUERDA_1, LOW); digitalWrite(DIRECAO_ESQUERDA_2, LOW); } while(false)
+#define FREIO() do { FREIO_DIREITA(); FREIO_ESQUERDA(); } while(false)
+
+// Velocidade armazenada PWM
+volatile int pwmDireita = 0;
+volatile int pwmEsquerda = 0;
+
+// Numero de passos do Encoder Ótico
+volatile int contador_direita = 0;
+volatile int contador_esquerda = 0;
+
+// Definir velocidades
+int velocidadeDireita  = 130;
+int velocidadeEsquerda = 130;
+
+void setup() {
+  // Configuração da Comunicação Serial
+  Serial.begin(9600);
+
+  // Configuração dos pinos da Ponte H
+  pinMode(DIRECAO_DIREITA_1, OUTPUT);
+  pinMode(DIRECAO_DIREITA_2, OUTPUT);
+  pinMode(DIRECAO_ESQUERDA_1, OUTPUT);
+  pinMode(DIRECAO_ESQUERDA_2, OUTPUT);
+
+  // Configuração dos pinos do Encoder Ótico
+  pinMode(ENCODER_DIREITA, INPUT_PULLUP);
+  pinMode(ENCODER_ESQUERDA, INPUT_PULLUP);
+
+  // Funções de Interrupção de cada um dos Encoders
+  attachInterrupt(digitalPinToInterrupt(ENCODER_DIREITA), contadorDireita, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_ESQUERDA), contadorEsquerda, CHANGE);
+}
+
+void loop() {
+  ACELERA_DIREITA(velocidadeDireita);
+  ACELERA_ESQUERDA(velocidadeEsquerda);
+  IR_PARA_FRENTE_DIREITA();
+  IR_PARA_FRENTE_ESQUERDA();
+  delay(1000);
+  FREIO();
+  delay(1500);
+}
+
+void contadorDireita(){
+  contador_direita++;
+}
+
+void contadorEsquerda(){
+  contador_esquerda++;
+}
